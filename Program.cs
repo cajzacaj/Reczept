826a982b5f09d52bb8d887982c9f4f1cb09d2c95
jenspalmqvist <jpalmqvi@gmail.com>
2019-07-24 10:10:14 +0200

    First commit! Such wow! Much good! So new!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReczeptBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
