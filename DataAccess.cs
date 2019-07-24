f9c0ec4604a5309a2fe2e47680e67b234e77fe3e
cajzacaj <cajzacaj@gmail.com>
2019-07-24 12:08:56 +0200

    Gjort de två första metoderna

--- DataAccess.cs
+++ DataAccess.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace ReczeptBot
 {
     internal class DataAccess
     {
+        private const string conString = "Server=(localdb)\\mssqllocaldb; Database=Reczept";
+
         internal List<Recipe> GetAllRecipes()
         {
-            throw new NotImplementedException();
+            var sql = @"SELECT [Id] [Name]
+                        FROM Recipe";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                var list = new List<Recipe>();
+
+                while (reader.Read())
+                {
+                    var recipe = new Recipe
+                    {
+                        Id = reader.GetSqlInt32(0).Value,
+                        Name = reader.GetSqlString(1).Value,
+                    };
+                    list.Add(recipe);
+                }
+
+                return list;
+
+            }
         }
 
-        internal void GetRecipeWithTag(string tag)
+        internal List<Recipe> GetAllRecipesWithTag(string tag)
         {
             throw new NotImplementedException();
         }
