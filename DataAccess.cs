98b53c906493715ac4020f7473b5dee922861bbd
cajzacaj <cajzacaj@gmail.com>
2019-07-24 14:10:48 +0200

    Snott metoder från igår

--- DataAccess.cs
+++ DataAccess.cs
@@ -73,22 +73,90 @@ namespace ReczeptBot
 
         internal void GetUserIdFromName(User user)
         {
-            throw new NotImplementedException();
+            var sql = @"SELECT Id
+                        FROM SlackUser 
+                        WHERE Name=@Name";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Name", user.Name));
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    user.MemberId = reader.GetSqlString(0).Value;
+                }
+            }
         }
 
         internal void GetTagId(Tag tag)
         {
-            throw new NotImplementedException();
+            var sql = @"SELECT Id
+                        FROM Tag 
+                        WHERE Name=@Name";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Name", tag.Name));
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    tag.Id = reader.GetSqlInt32(0).Value;
+                }
+            }
         }
 
         internal List<Tag> GetTagsForRecipe(Recipe recipe)
         {
-            throw new NotImplementedException();
+            var sql = @"SELECT t.id, t.Name FROM Tag t
+                        JOIN TagsOnRecipe tor ON t.Id=tor.TagId
+                        JOIN Recipe r ON tor.UserId = r.Id
+                        WHERE r.Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Id", recipe.Id));
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                var list = new List<Tag>();
+
+                while (reader.Read())
+                {
+                    var tag = new Tag
+                    {
+                        Id = reader.GetSqlInt32(0).Value,
+                        Name = reader.GetSqlString(1).Value,
+                    };
+                    list.Add(tag);
+                }
+
+                return list;
+            }
         }
 
         internal void AddUserLikesRecipe(Recipe recipe, User currentUser)
         {
-            throw new NotImplementedException();
+            var sql = "INSERT INTO UserLikesRecipe(UserId, RecipeId) VALUES(@UserId, @RecipeId)";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("UserId", currentUser.MemberId));
+                command.Parameters.Add(new SqlParameter("RecipeId", recipe.Id));
+
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
